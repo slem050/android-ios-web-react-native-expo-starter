d6f89e8af8991585a3bf8bb66302c8df379b0790
saleem sabeihe <sbsaleem76@gmail.com>
2024-12-08 19:11:29 +0200

    remove current project

--- __mocks__/@react-navigation/native.ts
+++
@@ -1,7 +0,0 @@
-export const mockGoBack = jest.fn();
-export const mockNavigate = jest.fn();
-
-export const useNavigation = () => ({
-  goBack: mockGoBack,
-  navigate: mockNavigate,
-});
--- types/index.ts
+++
@@ -1,8 +0,0 @@
-export type User = {
-  uid: string;
-  displayName: string | null;
-  email: string | null;
-  photoURL: string | null;
-  phoneNumber: string | null;
-  isAdmin: boolean;
-};
